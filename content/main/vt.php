1d3fae318381655503091439492337211ecc960f
nadya <nadya.wyn@ya.ru>
2020-10-05 00:06:04 +0500

    Added vitamins list to VT section

--- content/main/vt.php
+++ content/main/vt.php
@@ -1,8 +1,14 @@
-<section class="main__section main__section_ad">
+<section class="main__section main__section_vt">
 			<div class="container container_padh">
 				<h2 class="main__title">Vitamins</h2>
-				
 
+				<ul class="vitamins__list">
+					<li class="vitamins__item complete">B12 <span>Note for what this vitamin is</span> </li>
+					<li class="vitamins__item">D3, K2 + Iodine <span></span> </li>
+					<li class="vitamins__item">Ferrum <span></span></li>
+					<li class="vitamins__item">Omega3 <span></span></li>
+					<li class="vitamins__item">Magnesium <span></span></li>
+				</ul>
 			</div>
-		</div>
-</section>
+
+		</section>
