c6cf0d180762f481e376257f36df79663d75f96d
nadya <nadya.wyn@ya.ru>
2020-11-02 09:52:55 +0500

    Added output of FT report from database

--- content/report/rep_ft.php
+++ content/report/rep_ft.php
@@ -13,10 +13,18 @@
 					$i = "0".$i;
 				}
 
-				print('<div class="fitness__day"><div class="fitness__day_date">');
+				print('<div class="fitness__day" id="ftday'.$i.'"><div class="fitness__day_date">');
 				print $i;
 				print('</div><div class="fitness__day_res">');
-				print('');
+					//Выводим фитнес-занятия за текущую дату
+					$thisdatabasename = 'si__'.$myname.'_fttracker';
+					$sql_ftd = 'SELECT * FROM `'.$thisdatabasename.'` WHERE `recdate` = "2020-'.$today_m.'-'.$i.'"';
+					$result_ftd = mysqli_query($link, $sql_ftd);
+					while ($row_ftd = mysqli_fetch_array($result_ftd)) {
+						print('<div class="fitness__day_res-item fitness__res-item" ');
+						print('style="background-image:url(icons/fitness/'.$row_ftd['fitid'].'.png)">');
+						print('</div>');
+					}
 				print('</div></div>');
 			}
 		?>
