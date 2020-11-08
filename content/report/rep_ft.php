95ddf1ce83c8dd7ab6231ec7132b8c0ca265e5da
nadya <nadya.wyn@ya.ru>
2020-11-08 13:21:14 +0500

    Changed block order

--- content/report/rep_ft.php
+++ content/report/rep_ft.php
@@ -1,37 +1,7 @@
 <section class="main_report-section">
 	<h2 id="ft">Fitness</h2>
-		<h3 class="ad__title ad__title_cal">
-		<?php
-			echo $today_mw;
-		?>
-		</h3>
-
-		<div class="fitness__wrapper_output">
-		<?php
-			for($i=1; $i<=$enddate; $i++){
-				if ($i < 10) {
-					$i = "0".$i;
-				}
-
-				print('<div class="fitness__day" id="ftday'.$i.'"><div class="fitness__day_date">');
-				print $i;
-				print('</div><div class="fitness__day_res">');
-					//Выводим фитнес-занятия за текущую дату
-					$thisdatabasename = 'si__'.$myname.'_fttracker';
-					$sql_ftd = 'SELECT * FROM `'.$thisdatabasename.'` WHERE `recdate` = "2020-'.$today_m.'-'.$i.'"';
-					$result_ftd = mysqli_query($link, $sql_ftd);
-					while ($row_ftd = mysqli_fetch_array($result_ftd)) {
-						print('<div class="fitness__day_res-item fitness__res-item" ');
-						print('style="background-image:url(icons/fitness/'.$row_ftd['fitid'].'.png)">');
-						print('</div>');
-					}
-				print('</div></div>');
-			}
-		?>
-
-		</div>
-		<div class="fitness__output_body">
-		<h3 class="ad__title">My body progress</h3>
+		
+	<h3 class="ad__title ad__title_cal">My body progress</h3>
 		<div class="fitness__overall">
 			<div class="fitness__wrapper">
 				<div class="fitness__wrapper_out fitness__wrapper_date"></div>
@@ -160,6 +130,37 @@
 
 
 		</div>
-	</div>	
+	<h3 class="ad__title">
+		<?php
+			echo $today_mw;
+		?>
+		</h3>
+
+		<div class="fitness__wrapper_output">
+		<?php
+			for($i=1; $i<=$enddate; $i++){
+				if ($i < 10) {
+					$i = "0".$i;
+				}
+
+				print('<div class="fitness__day" id="ftday'.$i.'"><div class="fitness__day_date">');
+				print $i;
+				print('</div><div class="fitness__day_res">');
+					//Выводим фитнес-занятия за текущую дату
+					$thisdatabasename = 'si__'.$myname.'_fttracker';
+					$sql_ftd = 'SELECT * FROM `'.$thisdatabasename.'` WHERE `recdate` = "2020-'.$today_m.'-'.$i.'"';
+					$result_ftd = mysqli_query($link, $sql_ftd);
+					while ($row_ftd = mysqli_fetch_array($result_ftd)) {
+						print('<div class="fitness__day_res-item fitness__res-item" ');
+						print('style="background-image:url(icons/fitness/'.$row_ftd['fitid'].'.png)">');
+						print('</div>');
+					}
+				print('</div></div>');
+			}
+		?>
+
+		</div>
+	
+		
 		
 </section>
