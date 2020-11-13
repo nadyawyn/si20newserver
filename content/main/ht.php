3163b6d33f9b00e0564ff14b034b57f6db430eda
nadya <nadya.wyn@ya.ru>
2020-11-13 12:21:08 +0500

    Added correct output of Habit data from database

--- content/main/ht.php
+++ content/main/ht.php
@@ -12,8 +12,9 @@
 						<?php
 							$thisdatabasename = 'si__'.$myname.'_htnames';
 				
-							$sql_ht = 'SELECT * FROM `'.$thisdatabasename.'`';
+							$sql_ht = 'SELECT * FROM `'.$thisdatabasename.'` ORDER BY `habit_id`';
 							$result_ht = mysqli_query($link, $sql_ht);
+							$length = mysqli_num_rows($result_ht);
 
 							while($row_ht = mysqli_fetch_array($result_ht)){
 								print('<div class="habit__names-item">'.$row_ht['habit_name'].'</div>');
@@ -22,61 +23,64 @@
 						?>
 					</div>
 					<div class="habit__val">
-						<ul class="habit__val-ul" id="val-ul-1">
-							<li class="habit__val-li">1</li>
-							<li class="habit__val-li"></li>
-							<li class="habit__val-li"></li>
-						</ul>
-						<ul class="habit__val-ul" id="val-ul-2">
-							<li class="habit__val-li complete">2</li>
-							<li class="habit__val-li"></li>
-							<li class="habit__val-li"></li>
-						</ul>
-						<ul class="habit__val-ul" id="val-ul-3">
-							<li class="habit__val-li">3</li>
-							<li class="habit__val-li complete"></li>
-							<li class="habit__val-li"></li>
-						</ul>
-						<ul class="habit__val-ul" id="val-ul-4">
-							<li class="habit__val-li">4</li>
-							<li class="habit__val-li"></li>
-							<li class="habit__val-li"></li>
-						</ul>
-						<ul class="habit__val-ul" id="val-ul-1">
-							<li class="habit__val-li">5</li>
-							<li class="habit__val-li"></li>
-							<li class="habit__val-li"></li>
-						</ul>
-						<ul class="habit__val-ul" id="val-ul-2">
-							<li class="habit__val-li">6</li>
-							<li class="habit__val-li"></li>
-							<li class="habit__val-li"></li>
-						</ul>
-						<ul class="habit__val-ul" id="val-ul-3">
-							<li class="habit__val-li">7</li>
-							<li class="habit__val-li"></li>
-							<li class="habit__val-li"></li>
-						</ul>
-						<ul class="habit__val-ul" id="val-ul-4">
-							<li class="habit__val-li">8</li>
-							<li class="habit__val-li"></li>
-							<li class="habit__val-li"></li>
-							<li class="habit__val-li"></li>
-						</ul>
-						<ul class="habit__val-ul" id="val-ul-4">
-							<li class="habit__val-li">9</li>
-							<li class="habit__val-li"></li>
-							<li class="habit__val-li"></li>
-							<li class="habit__val-li"></li>
-							<li class="habit__val-li"></li>
-						</ul>
-						<ul class="habit__val-ul" id="val-ul-4">
-							<li class="habit__val-li">10</li>
-							<li class="habit__val-li"></li>
-							<li class="habit__val-li"></li>
-							<li class="habit__val-li"></li>
-							<li class="habit__val-li"></li>
-						</ul>
+					<!-- Смотрим, есть ли запись за текущую дату -->
+						<?php
+							$thisdatabasename1 = 'si__'.$myname.'_htdata';
+				
+							$sql_htt = 'SELECT * FROM `'.$thisdatabasename1.'` WHERE `recdate` = "'.$fulldatetoday.'"';
+							$result_htt = mysqli_query($link, $sql_htt);
+
+							$row_htt = mysqli_fetch_array($result_htt);
+
+							if(!$row_htt) {
+								for($j = 1; $j <= $length; $j++) {
+									$sql_emptylines = 'INSERT INTO `'.$thisdatabasename1.'` SET `recdate` = "'. $fulldatetoday .'", habitid = "' . $j . '", habitres = "0"';
+									$result_emptylines = mysqli_query($link, $sql_emptylines);
+								}
+							}
+						?>
+
+<!-- Выводим все данные привычек до сегодняшнего дня месяца -->
+						<?php
+							for($i = 1; $i <= $today_d; $i++) {
+								//$sql_hab = 'SELECT * FROM `'.$thisdatabasename1.'` WHERE recdate BETWEEN "'.$today_y.'-'.$today_m.'-01" AND "'.$today_y.'-'.$today_m.'-'.$enddate.'"';
+
+								if ($i < 10) {
+									$i = '0'.$i;
+								}
+								
+								//CHECK if there are records for this day!
+								$sql_check = 'SELECT * FROM `'.$thisdatabasename1.'` WHERE recdate = "'.$today_y.'-'.$today_m.'-'.$i.'"';
+								$result_check = mysqli_query($link, $sql_check);
+								$row_check = mysqli_fetch_array($result_check);
+
+								if($row_check) {
+									$sql_hab = 'SELECT * FROM `'.$thisdatabasename1.'` WHERE recdate = "'.$today_y.'-'.$today_m.'-'.$i.'"';
+									$result_hab = mysqli_query($link, $sql_hab);
+
+										print('<ul class="habit__val-ul" id="val-ul-'.$i.'">');
+
+											while ($row_hab = mysqli_fetch_array($result_hab)) {
+												$thisday = substr($row_hab['recdate'], -2);
+												print('<li class="habit__val-li ');
+												if($row_hab['habitres'] == 1) {
+													print('complete');
+												}
+												print('" id="hab'.$row_hab['habitid'].'">');
+													if($row_hab['habitid'] == 1){
+														print($thisday);
+													}
+												print('</li>');
+											}
+										print('</ul>');
+								}
+							}
+
+							
+
+						?>
+						
+						
 
 
 					</div>
